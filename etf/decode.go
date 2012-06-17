332b0adf2031132f11b6dfad78f6077e88acbfef
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-06-18 01:18:07 +0200

    initial Decode()

--- etf/decode.go
+++ etf/decode.go
@@ -23,6 +23,183 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+import (
+  "fmt"
+  . "reflect"
+)
+
+var (
+  atomType = ValueOf(Atom("")).Type()
+)
+
+// OverflowError is returned when number cannot be represented by supplied type.
+type OverflowError struct {
+  Value interface{}
+  Type  Type
+}
+
+// TypeError is returned when a type cannot be decoded.
+type TypeError struct {
+  Type Type
+}
+
+// VersionError is returned on invalid Erlang external format version number.
+type VersionError struct {
+  Version byte
+}
+
+func (err OverflowError) Error() string {
+  return fmt.Sprintf("overflow error: cannot represent %s by type %s", err.Value, err.Type)
+}
+
+func (err TypeError) Error() string {
+  return fmt.Sprintf("type error: cannot represent type %s", err.Type)
+}
+
+func (err VersionError) Error() string {
+  return fmt.Sprintf("version error: version %d is not supported", err.Version)
+}
+
+// decodeStruct decodes a structure.
+func decodeStruct(b []byte, ptr Value) (size uint, err error) {
+  var arity int
+
+  v := ptr.Elem()
+
+  switch erlType(b[0]) {
+  case erlSmallTuple:
+    // $hA…
+    if len(b) >= 2 {
+      arity = int(b[1])
+      size = 2
+      goto decode
+    } else {
+      err = StructuralError{
+        fmt.Sprintf("invalid tuple length (%d)", len(b)),
+      }
+    }
+
+  case erlLargeTuple:
+    // $iAAAA…
+    if len(b) >= 5 {
+      arity = int(be.Uint32(b[1:5]))
+      size = 5
+      goto decode
+    } else {
+      err = StructuralError{
+        fmt.Sprintf("invalid tuple length (%d)", len(b)),
+      }
+    }
+
+  default:
+    err = SyntaxError{"not a tuple"}
+  }
+
+  return
+
+decode:
+  if arity != v.NumField() {
+    err = StructuralError{fmt.Sprintf("different number of fields (%d, should be %d)", v.NumField(), arity)}
+    return
+  }
+
+  for i := 0; i < arity; i++ {
+    var s uint
+    f := v.Field(i).Addr()
+
+    s, err = decode(b[size:], f)
+    size += s
+
+    if err != nil {
+      break
+    }
+  }
+
+  return
+}
+
+func decode(b []byte, ptr Value) (size uint, err error) {
+  v := ptr.Elem()
+
+  switch v.Kind() {
+  case Bool:
+    var result bool
+    if result, size, err = parseBool(b); err == nil {
+      v.SetBool(result)
+    }
+
+  case Int, Int8, Int16, Int32, Int64:
+    var result int64
+    if result, size, err = parseInt64(b); err == nil {
+      if v.OverflowInt(result) {
+        err = OverflowError{result, v.Type()}
+      } else {
+        v.SetInt(result)
+      }
+    }
+
+  case Uint, Uint8, Uint16, Uint32, Uint64:
+    var result uint64
+    if result, size, err = parseUint64(b); err == nil {
+      if v.OverflowUint(result) {
+        err = OverflowError{result, v.Type()}
+      } else {
+        v.SetUint(result)
+      }
+    }
+
+  case Float32:
+    var result float64
+    if result, size, err = parseFloat64(b); err == nil {
+      if v.OverflowFloat(result) {
+        err = OverflowError{result, v.Type()}
+      } else {
+        v.SetFloat(result)
+      }
+    }
+
+  case Float64:
+    var result float64
+    if result, size, err = parseFloat64(b); err == nil {
+      v.SetFloat(result)
+    }
+
+  case String:
+    if v.Type() == atomType {
+      var result Atom
+      if result, size, err = parseAtom(b); err == nil {
+        v.Set(ValueOf(result))
+      }
+    } else {
+      var result string
+      if result, size, err = parseString(b); err == nil {
+        v.Set(ValueOf(result))
+      }
+    }
+
+  case Struct:
+    size, err = decodeStruct(b, ptr)
+
+  default:
+    err = TypeError{v.Type()}
+  }
+
+  return
+}
+
+// Decode unmarshals a value and stores it to a variable pointer by ptr.
+func Decode(b []byte, ptr interface{}) (size uint, err error) {
+  if b[0] != erlFormatVersion {
+    err = VersionError{b[0]}
+  } else {
+    p := ValueOf(ptr)
+    size, err = decode(b[1:], p)
+    size++
+  }
+
+  return
+}
+
 // Local Variables:
 // indent-tabs-mode: nil
 // tab-width: 2
---
+++ etf/decode_test.go
@@ -0,0 +1,84 @@
+package etf
+
+/*
+Copyright © 2012 Serge Zirukin
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import (
+  "github.com/bmizerany/assert"
+  "testing"
+)
+
+func Test_Decode(t *testing.T) {
+  var s string
+  size, err := Decode([]byte{131,107,0,3,49,50,51}, &s)
+  assert.Equal(t, nil, err)
+  assert.Equal(t, uint(7), size)
+  assert.Equal(t, "123", s)
+
+  type testStruct struct {
+    Atom
+    X uint8
+    S string
+  }
+
+  var ts testStruct
+
+  size, err = Decode([]byte{
+    131,104,3,100,0,4,98,108,97,104,97,4,108,0,0,0,4,98,
+    0,0,4,68,98,0,0,4,75,98,0,0,4,50,98,0,0,4,48,106,
+  }, &ts)
+  assert.Equal(t, nil, err)
+  assert.Equal(t, uint(38), size)
+  assert.Equal(t, uint8(4), ts.X)
+  assert.Equal(t, "фыва", ts.S)
+
+  size, err = Decode([]byte{
+    131,104,3,99,50,46,57,57,57,57,57,57,57,57,57,57,57,57,57,
+    57,57,56,56,56,57,56,101,45,48,49,0,0,0,0,0,97,4,108,0,0,
+    0,4,98,0,0,4,68,98,0,0,4,75,98,0,0,4,50,98,0,0,4,48,106,
+  }, &ts)
+  assert.NotEqual(t, nil, err)
+
+  type testStruct2 struct {
+    T testStruct
+    Y int
+  }
+
+  var ts2 testStruct2
+
+  size, err = Decode([]byte{
+    131,104,2,104,3,100,0,4,98,108,97,104,97,4,108,0,0,0,4,98,
+    0,0,4,68,98,0,0,4,75,98,0,0,4,50,98,0,0,4,48,106,98,0,0,2,154,
+  }, &ts2)
+  assert.Equal(t, nil, err)
+  assert.Equal(t, uint(45), size)
+  assert.Equal(t, uint8(4), ts2.T.X)
+  assert.Equal(t, "фыва", ts2.T.S)
+  assert.Equal(t, 666, ts2.Y)
+}
+
+// Local Variables:
+// indent-tabs-mode: nil
+// tab-width: 2
+// End:
+// ex: set tabstop=2 shiftwidth=2 expandtab:
