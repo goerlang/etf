6369688793d4479792f3b61208289f6cb0755f24
Maxim Treskin <zerthurd@gmail.com>
2013-01-24 01:15:32 +0700

    Fix incorrect reading

--- read/read.go
+++ read/read.go
@@ -298,13 +298,13 @@ func ruint8(r io.Reader) (uint8, error) {
 func ruint16(r io.Reader) (uint16, error) {
 	b := []byte{0, 0}
 	_, err := io.ReadFull(r, b)
-	return uint16((b[0] << 8) | b[1]), err
+	return binary.BigEndian.Uint16(b), err
 }
 
 func ruint32(r io.Reader) (uint32, error) {
 	b := []byte{0, 0, 0, 0}
 	_, err := io.ReadFull(r, b)
-	return uint32((b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]), err
+	return binary.BigEndian.Uint32(b), err
 }
 
 func buint8(r io.Reader) ([]byte, error) {
